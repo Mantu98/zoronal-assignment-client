e272453c475ea7738a477f78d69dc8f214c39d05
Mantu98 <mantu07941@gmail.com>
2026-05-24 22:59:51 +0530

    feat: setup frontend folder structure and dependencies

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen text-3xl font-bold">
+      Zoronal Assignment
+    </div>
+  );
+}
+
+export default App;
